3aaa6fde539bde08afcc112c5e72b3886ab4fe66
caele <tsm@qlik.com>
2019-09-25 15:37:47 +0200

    feat: connect ext def to nebula def

--- src/ext.js
+++ src/ext.js
@@ -1,47 +1,46 @@
-export default {
-  definition: {
-    type: 'items',
-    component: 'accordion',
-    items: {
-      data: {
-        uses: 'data',
-      },
-      settings: {
-        type: 'items',
-        translation: 'Settings',
-        items: {
-          styling: {
-            component: 'style-editor',
-            translation: 'LayerStyleEditor.component.styling',
-            subtitle: 'LayerStyleEditor.component.styling',
-            resetBtnTranslation: 'LayerStyleEditor.component.resetAll',
-            key: 'content',
-            ref: 'components',
-            defaultValues: {
+export default function ext(/* env */) {
+  return {
+    definition: {
+      type: 'items',
+      component: 'accordion',
+      items: {
+        settings: {
+          type: 'items',
+          translation: 'Settings',
+          items: {
+            styling: {
+              component: 'style-editor',
+              translation: 'LayerStyleEditor.component.styling',
+              subtitle: 'LayerStyleEditor.component.styling',
+              resetBtnTranslation: 'LayerStyleEditor.component.resetAll',
               key: 'content',
-              style: {
-                fontColor: {
-                  color: '#aeaeae',
-                  index: -1,
-                },
-                fontSize: 15,
-              },
-            },
-            items: {
-              content: {
-                components: 'items',
-                type: 'items',
-                items: {
+              ref: 'components',
+              defaultValues: {
+                key: 'content',
+                style: {
                   fontColor: {
-                    component: 'color-picker',
+                    color: '#aeaeae',
+                    index: -1,
                   },
-                  fontSize: {
-                    show: true,
-                    component: 'slider',
-                    min: 4,
-                    max: 48,
-                    step: 1,
-                    defaultValue: 15,
+                  fontSize: 15,
+                },
+              },
+              items: {
+                content: {
+                  components: 'items',
+                  type: 'items',
+                  items: {
+                    fontColor: {
+                      component: 'color-picker',
+                    },
+                    fontSize: {
+                      show: true,
+                      component: 'slider',
+                      min: 4,
+                      max: 48,
+                      step: 1,
+                      defaultValue: 15,
+                    },
                   },
                 },
               },
@@ -50,11 +49,11 @@ export default {
         },
       },
     },
-  },
-  support: {
-    export: !1,
-    exportData: !1,
-    snapshot: !1,
-    viewData: !1,
-  },
-};
+    support: {
+      export: !1,
+      exportData: !1,
+      snapshot: !1,
+      viewData: !1,
+    },
+  };
+}
