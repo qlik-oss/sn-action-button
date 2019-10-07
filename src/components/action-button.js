982adc483b387ef30ce7c402ae83311ce241009d
Max Gefvert <max.gefvert@qlik.com>
2019-10-07 13:56:44 +0200

    fix: correct defaults

--- src/components/action-button.js
+++ src/components/action-button.js
@@ -8,18 +8,18 @@ const setStyle = (styleString, path, setting) => {
 
 const setStyleColor = (styleString, path, inputColor, defaultColor) => {
   const color = themeResolver.resolveColor(inputColor, palette);
-  styleString += `${path}: ${color === 'null' ? defaultColor : color};`;
+  styleString += `${path}: ${color === 'none' ? defaultColor : color};`;
   return styleString;
 };
 
 export default function ActionButton({ layout, button, Theme }) {
   const { style } = layout;
   palette = Theme.getCurrent().properties.palettes.ui[0].colors;
-  let styles = 'width: 100%; height: 100%;';
+  let styles = 'width: 100%; height: 100%; font-weight: bold;';
   if (style) {
-    styles = setStyleColor(styles, 'color', style.fontColor || null, '#ffffff');
-    styles = setStyle(styles, 'font-size', style.fontSize ? `${style.fontSize}px` : '12px');
-    styles = setStyleColor(styles, 'background-color', style.backgroundColor || null, '#3F8AB3');
+    styles = setStyleColor(styles, 'color', style.fontColor, '#ffffff');
+    styles = setStyle(styles, 'font-size', style.fontSize ? `${style.fontSize}px` : '13px');
+    styles = setStyleColor(styles, 'background-color', style.backgroundColor, '#3F8AB3');
   } else {
     styles += 'color: #ffffff; font-size: 12px; background-color: #3F8AB3';
   }
