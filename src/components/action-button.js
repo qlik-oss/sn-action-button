3c29e15fc564fbc98182b834c0a7906cc5d78462
Max Gefvert <max.gefvert@qlik.com>
2019-10-08 15:41:49 +0200

    fix: styling logic and unit tests (#17)

--- src/components/action-button.js
+++ src/components/action-button.js
@@ -1,30 +1,10 @@
-import themeResolver from '../utils/theme-resolver';
-
-let palette;
-const setStyle = (styleString, path, setting) => {
-  styleString += `${path}: ${setting};`;
-  return styleString;
-};
-
-const setStyleColor = (styleString, path, inputColor, defaultColor) => {
-  const color = themeResolver.resolveColor(inputColor, palette);
-  styleString += `${path}: ${color === 'null' ? defaultColor : color};`;
-  return styleString;
-};
+import styleFormatter from '../utils/style-formatter';
 
 export default function ActionButton({ layout, button, Theme }) {
   const { style } = layout;
-  palette = Theme.getCurrent().properties.palettes.ui[0].colors;
-  let styles = 'width: 100%; height: 100%;';
-  if (style) {
-    styles = setStyleColor(styles, 'color', style.fontColor || null, '#ffffff');
-    styles = setStyle(styles, 'font-size', style.fontSize ? `${style.fontSize}px` : '12px');
-    styles = setStyleColor(styles, 'background-color', style.backgroundColor || null, '#3F8AB3');
-  } else {
-    styles += 'color: #ffffff; font-size: 12px; background-color: #3F8AB3';
-  }
-  button.setAttribute('style', styles);
-  button.textContent = (style && style.label) || 'My Button';
+  const formattedStyles = styleFormatter.getStyles(style, Theme);
+  button.setAttribute('style', formattedStyles);
+  button.textContent = style && style.label;
   button.onclick = () => {
     // perform action stack
   };
