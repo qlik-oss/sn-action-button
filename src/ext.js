6c736452e645124d7fb040f510261e771af7d130
niekvanstaveren <niek.vanstaveren@qlik.com>
2019-10-09 10:37:16 +0200

    fix: remove placeholder

--- src/ext.js
+++ src/ext.js
@@ -79,7 +79,6 @@ export default function ext(/* env */) {
                   component: 'string',
                   ref: 'style.label',
                   translation: 'Label',
-                  placeholder: 'My button',
                 },
               ],
             },
