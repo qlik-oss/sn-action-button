77f2e1a99b1891af81ce9e2245d682234f4d23c1
niekvanstaveren <niek.vanstaveren@qlik.com>
2019-10-14 12:16:58 +0200

    fix: set default properties for style

--- src/object-properties.js
+++ src/object-properties.js
@@ -1,8 +1,40 @@
 const properties = {
+  /**
+   * Current version of this generic object definition
+   * @type {string}
+   */
+  version: '0.1.0',
+  /**
+   * @type {boolean}
+   */
   showTitles: false,
+  /**
+   * @type {string}
+   */
   title: '',
+  /**
+   * @type {string}
+   */
   subtitle: '',
+  /**
+   * @type {string}
+   */
   footnote: '',
+  /**
+   * @typedef {object}
+   */
+  style: {
+    fontSize: 12,
+    fontColor: {
+      index: -1,
+      color: '#ffffff',
+    },
+    backgroundColor: {
+      index: -1,
+      color: '#3F8AB3',
+    },
+    label: null,
+  },
 };
 
 export default properties;
