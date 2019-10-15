39f2f00e4a996c7b975322c8be336faeb7f423e5
Andreas Jonsson <aoj@qlik.com>
2019-10-15 10:42:14 +0200

    test: add intial rendering test

---
+++ test/integration/setup.spec.js
@@ -0,0 +1,22 @@
+const serve = require('@nebula.js/cli-serve'); // eslint-disable-line
+
+let s;
+
+before(async function run() {
+  this.timeout(10000);
+  s = await serve({
+    build: false,
+    open: false,
+  });
+
+  process.testServer = s;
+
+  page.on('pageerror', e => {
+    // eslint-disable-next-line no-console
+    console.log('Error:', e.message, e.stack);
+  });
+});
+
+after(() => {
+  s.close();
+});
