0e16853499b7cf6a9dbba6bd1310621dd0e175e1
Max Gefvert <max.gefvert@qlik.com>
2019-10-15 15:11:22 +0200

    test: tests for getting options and action title

--- src/__tests__/ext.spec.js
+++ src/__tests__/ext.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-destructuring */
 import ext, { checkShow } from '../ext';
 
 describe('ext', () => {
@@ -22,7 +23,86 @@ describe('ext', () => {
   });
 
   describe('ext', () => {
-    const result = ext();
-    expect(result).to.be.an('object');
+    const props = ext();
+
+    it('should return properties object', () => {
+      expect(props).to.be.an('object');
+    });
+
+    describe('itemTitleRef', () => {
+      const itemTitleRef = props.definition.items.actions.itemTitleRef;
+
+      it('Should return action label', () => {
+        data = { actionType: 'applyBookmark' };
+        const itemTitle = itemTitleRef(data, 0);
+        expect(itemTitle).to.equal('Apply a bookmark');
+      });
+
+      it('Should return default action label when no act.label', () => {
+        data = { actionType: 'someAction' };
+        const itemTitle = itemTitleRef(data, 0);
+        expect(itemTitle).to.equal('Action 1');
+      });
+    });
+
+    describe('options', () => {
+      const items = props.definition.items.actions.items;
+      const bookmarks = [
+        {
+          qData: {
+            title: 'someBookmark',
+          },
+          qInfo: {
+            qId: 'someId',
+          },
+        },
+      ];
+      const fields = [
+        {
+          qName: 'someField',
+        },
+      ];
+      const variables = [
+        {
+          qName: 'someVariable',
+          qIsReserved: false,
+        },
+        {
+          qName: 'someSystemVariable',
+          qIsReserved: true,
+        },
+      ];
+
+      const handler = {
+        app: {
+          getBookmarkList: () => bookmarks,
+          getFieldList: () => fields,
+          getVariableList: () => variables,
+        },
+      };
+
+      it('Should return an array with bookmarks', async () => {
+        const bookmarkOptions = await items.bookmark.options(null, handler);
+        expect(bookmarkOptions).to.have.length(1);
+        expect(bookmarkOptions[0]).to.be.an('object');
+      });
+
+      it('Should return an array with fields', async () => {
+        const bookmarkOptions = await items.bookmark.options(null, handler);
+        expect(bookmarkOptions).to.have.length(1);
+        expect(bookmarkOptions[0]).to.be.an('object');
+      });
+
+      it('Should return an array with a single non-system variable', async () => {
+        const bookmarkOptions = await items.variable.options({ showSystemVariables: false }, handler);
+        expect(bookmarkOptions).to.have.length(1);
+        expect(bookmarkOptions[0]).to.be.an('object');
+      });
+
+      it('Should return an array with a single all variables', async () => {
+        const bookmarkOptions = await items.variable.options({ showSystemVariables: true }, handler);
+        expect(bookmarkOptions).to.have.length(2);
+      });
+    });
   });
 });
