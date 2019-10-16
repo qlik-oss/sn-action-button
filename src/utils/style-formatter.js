6c8edde934984b7a812ebc64fc2b1c6ea036cdd8
Elise Eborn <fdq@eu-fdq.qliktech.com>
2019-10-16 10:32:48 +0200

    refactor: fixed linting issue

--- src/utils/style-formatter.js
+++ src/utils/style-formatter.js
@@ -38,18 +38,17 @@ export default {
     styles += formatColorProperty('color', style.fontColor, '#ffffff');
     styles += formatProperty('font-size', style.fontSize ? `${style.fontSize}px` : '12px');
     styles += formatColorProperty('background-color', style.backgroundColor, '#3F8AB3');
-    
     if (style && style.background && style.background.isUsed) {
-        let bgUrl = style.background.url.qStaticContentUrl.qUrl;
-        bgUrl.replace('http://localhost:4848/', '').replace(/^\.\.\//i, '/');
-        bgUrl = bgUrl.replace(/"/g, '\\"');
-        bgUrl = bgUrl.replace(/'/g, "\\'");
-        const bgImg = `http://localhost:4848/${(bgUrl[0] === '/' ? bgUrl.substr(1) : bgUrl)}`;
-        styles += formatProperty('background-image', `url('${bgImg}')`);
-        styles += formatProperty('background-size', backgroundSize[style.background.size] || backgroundSize.auto);
-        styles += formatProperty('background-position', backgroundPosition[style.background.position] || backgroundPosition.topLeft);
-        styles += formatProperty('background-repeat', 'no-repeat');
-     }
+      let bgUrl = style.background.url.qStaticContentUrl.qUrl;
+      bgUrl.replace('http://localhost:4848/', '').replace(/^\.\.\//i, '/');
+      bgUrl = bgUrl.replace(/"/g, '\\"');
+      bgUrl = bgUrl.replace(/'/g, "\\'");
+      const bgImg = `http://localhost:4848/${(bgUrl[0] === '/' ? bgUrl.substr(1) : bgUrl)}`;
+      styles += formatProperty('background-image', `url('${bgImg}')`);
+      styles += formatProperty('background-size', backgroundSize[style.background.size] || backgroundSize.auto);
+      styles += formatProperty('background-position', backgroundPosition[style.background.position] || backgroundPosition.topLeft);
+      styles += formatProperty('background-repeat', 'no-repeat');
+    }
 
     return styles;
   },
