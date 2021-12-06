de93f912be1fd4f5740d719e8bb553a0ee3b21ba
Max Gefvert <max.gefvert@qlik.com>
2021-12-06 08:42:46 +0100

    fix: move defaults props to object-properties

--- src/object-properties.js
+++ src/object-properties.js
@@ -1,3 +1,5 @@
+import DEFAULTS from './style-defaults';
+
 const properties = {
   /**
    * @type {boolean}
@@ -32,7 +34,43 @@ const properties = {
   /**
    * @type {object}
    */
-  style: {},
+  style: {
+    label: DEFAULTS.LABEL,
+    font: {
+      useColorExpression: false,
+      color: DEFAULTS.COLOR,
+      colorExpression: '',
+      size: DEFAULTS.FONT_SIZE,
+      style: DEFAULTS.FONT_SIZE,
+      align: DEFAULTS.TEXT_ALIGN,
+    },
+    background: {
+      useColorExpression: false,
+      color: DEFAULTS.COLOR,
+      colorExpression: '',
+      useImage: false,
+      url: {
+        qStaticContentUrlDef: {
+          qUrl: '',
+        },
+      },
+      size: DEFAULTS.BACKGROUND_SIZE,
+      position: DEFAULTS.BACKGROUND_POSITION,
+    },
+    border: {
+      useColorExpression: false,
+      color: DEFAULTS.COLOR,
+      colorExpression: '',
+      useBorder: false,
+      radius: DEFAULTS.BORDER_RADIUS,
+      width: DEFAULTS.BORDER_WIDTH,
+    },
+    icon: {
+      useIcon: false,
+      iconType: '',
+      position: DEFAULTS.ICON_POSITION,
+    },
+  },
   /**
    * @type {array}
    */
