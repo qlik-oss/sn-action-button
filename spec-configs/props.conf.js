5d04467f3fd276eba67c5f222e7063b85a19e2b3
Max Gefvert <max.gefvert@gmail.com>
2021-12-14 10:19:46 +0100

    docs: add jsdocs for all properties (#249)

--- spec-configs/props.conf.js
+++ spec-configs/props.conf.js
@@ -1,29 +1,18 @@
-const fs = require('fs');
 const path = require('path');
-
-const f = fs.readFileSync(path.resolve(__dirname, '../src/object-properties.js'), { encoding: 'utf-8' });
-const V_RX = /version: '(.+)',/;
-const v = V_RX.exec(f);
-
-if (!v) {
-  throw new Error('Could not find a version in properties');
-}
 
 const pkg = require(path.resolve(__dirname, '../package.json')); // eslint-disable-line
 
 module.exports = {
-  glob: [
-    './src/object-properties.js',
-  ],
+  glob: ['./src/object-properties.js'],
   package: path.resolve(__dirname, '../package.json'),
   api: {
-    stability: 'experimental',
+    stability: 'stable',
     properties: {
       'x-qlik-visibility': 'public',
     },
     visibility: 'public',
     name: `${pkg.name}:properties`,
-    version: v[1],
+    version: pkg.version,
     description: 'Action button generic object definition',
   },
   output: {
@@ -31,8 +20,7 @@ module.exports = {
   },
   parse: {
     types: {
-      NxCalcCond: {},
-      StringExpression: {},
+      'qae.GenericObjectProperties': {},
     },
   },
 };
