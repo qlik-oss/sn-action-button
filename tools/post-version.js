ca35b0ed3eae024ebd686e365bfe5fd8b6ab2571
Max Gefvert <max.gefvert@qlik.com>
2021-12-16 13:25:33 +0100

    fix: bump qext

--- tools/post-version.js
+++ tools/post-version.js
@@ -1,7 +1,30 @@
 #! /usr/bin/env node
 
 const { execSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+// const yargs = require('yargs');
 
+// const args = yargs(process.argv.slice(2)).argv;
+
+// bump qext file
+const p = path.resolve(process.cwd(), 'package.json');
+const pkg = require(p); // eslint-disable-line import/no-dynamic-require
+const vizName = pkg.name.replace('@nebula.js/', '');
+const contents = {
+  name: vizName,
+  description: pkg.description,
+  author: pkg.author,
+  icon: 'asdf',
+  type: 'visualization',
+  version: pkg.version,
+  supernova: true,
+};
+
+fs.writeFileSync(path.resolve(process.cwd(), `assets/${vizName}.qext`), JSON.stringify(contents, null, 2));
+
+// bump spec file
 execSync('yarn spec');
-execSync('git commit -am  "chore: bump spec version"');
-execSync('git push');
+
+// Commit and push. If this is done master, circle will automatically release
+// execSync('git commit -am  "chore: bump spec and qext version");
