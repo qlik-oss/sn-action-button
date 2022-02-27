c5d1a31b46efd24225150de9d0358ea6495703eb
goldbergjeffrey <goldbergjeffrey@yahoo.com>
2022-02-26 20:49:55 -0500

    fix: updated filter array logic

--- src/ext.js
+++ src/ext.js
@@ -165,12 +165,14 @@ export default function ext({ translator, isEnabled, senseNavigation }) {
                     const automations = await fetch('../api/v1/items?resourceType=automation&limit=100')
                       .then((response) => response.json());
                     const promises = automations.data.filter(async (blend) => {
-                      const autoInfo = await fetch(`../api/v1/automations/${blend.resourceId}`)
-                        .then((response) => response.json());
-                      if (autoInfo.run_mode.toLowerCase() === 'triggered') {
-                        return true;
-                      }
-                      return false;
+                      await fetch(`../api/v1/automations/${blend.resourceId}`)
+                        .then((response) => response.json())
+                        .then((info) => {
+                          if (info.run_mode === 'triggered') {
+                            return true;
+                          }
+                          return false;
+                        });
                     }).map((item) => {
                       const autoPath = `../api/v1/automations/${item.guid}/actions/execute?X-Execution-Token=${item.execution_token}`;
                       return {
