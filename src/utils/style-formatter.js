6fe8d398db37b5bcf9e613315c6201a8dedbbb40
Li Kang <kkllpaul7766@gmail.com>
2022-03-17 16:37:10 +0100

    fix: center button within its parent element

--- src/utils/style-formatter.js
+++ src/utils/style-formatter.js
@@ -39,7 +39,8 @@ const getColor = ({ useColorExpression, colorExpression, color }, defaultColor, 
 
 export default {
   getStyles({ style, disabled, theme, element }) {
-    let styles = 'width: 100%;height: 100%;transition: transform .1s ease-in-out;position: absolute;';
+    let styles =
+      'width: 100%;height: 100%;transition: transform .1s ease-in-out;position: absolute;bottom: 0;left: 0; top: 0;right: 0;margin: auto;';
     const { font, background, border } = style;
     const primaryColor = theme.getDataColorSpecials().primary;
     // enable
