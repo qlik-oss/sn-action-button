1ff3732762f391de555cc5bda42d3f416728cb73
Niek van Staveren <niek.vanstaveren@qlik.com>
2023-03-06 20:40:41 +0100

    chore: set unit test threshold

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   testEnvironment: 'jsdom',
   testRegex: 'src/.+\\.(test|spec)\\.[jt]sx?$',
+  coverageThreshold: {
+    global: {
+      lines: 78,
+    },
+  },
+  coveragePathIgnorePatterns: ['src/__tests__/default-button-props.js'],
 };
