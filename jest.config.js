db037307e3efa38d7ad80ac52d4713d79685a4bc
Niek van Staveren <Niek.vanstaveren@qlik.com>
2023-03-07 12:53:40 +0100

    chore: set unit test threshold (#360)
    * chore: set unit test threshold
    
    * chore: only collect coverage src folder
    
    * chore: exclude files from ut coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   testEnvironment: 'jsdom',
   testRegex: 'src/.+\\.(test|spec)\\.[jt]sx?$',
+  coverageThreshold: {
+    global: {
+      lines: 78,
+    },
+  },
+  coveragePathIgnorePatterns: [
+    'src/__tests__/default-button-props.js',
+    'src/data.js',
+    'src/utils/lui-icons',
+    'src/style-defaults',
+  ],
+  collectCoverageFrom: ['src/**'],
 };
