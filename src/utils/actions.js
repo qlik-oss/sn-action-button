a4db517c0adbe2f98f0bfc6ca1a5eb8b0c5d64b7
Amelia <bzn@qlik.com>
2023-05-24 08:45:09 +0200

    fix: use prettier

--- src/utils/actions.js
+++ src/utils/actions.js
@@ -33,6 +33,10 @@ const actions = [
       ({ app, bookmark }) =>
       async () => {
         const bookMarks = await app.getBookmarkList();
+        const findBmWithID = bookMarks.find((bm) => bm.qInfo.qId === bookmark);
+        if (findBmWithID.qData.qBookmark.qPatches.length > 0 || findBmWithID.qMeta.isExtended) {
+          app.clearAllSoftPatches();
+        }
         const findBm = bookMarks.find((bm) => bm.qData.title === bookmark);
         bookmark && (await app.applyBookmark((findBm && findBm.qInfo && findBm.qInfo.qId) || bookmark));
       },
