51df529302a2f604e7cbdddd6ef11143bd5fa967
Niek van Staveren <niek.vanstaveren@qlik.com>
2023-09-18 12:17:46 +0200

    chore: update scriptappy

--- spec-configs/props.conf.js
+++ spec-configs/props.conf.js
@@ -3,24 +3,28 @@ const path = require("path");
 const pkg = require(path.resolve(__dirname, "../package.json")); // eslint-disable-line
 
 module.exports = {
-  glob: ["./src/object-properties.js"],
-  package: path.resolve(__dirname, "../package.json"),
-  api: {
-    stability: "stable",
-    properties: {
-      "x-qlik-visibility": "public",
+  fromJsdoc: {
+    glob: ["./src/object-properties.js"],
+    package: path.resolve(__dirname, "../package.json"),
+    api: {
+      stability: "stable",
+      properties: {
+        "x-qlik-visibility": "public",
+      },
+      visibility: "public",
+      name: `${pkg.name}:properties`,
+      version: pkg.version,
+      description: "Action button generic object definition",
     },
-    visibility: "public",
-    name: `${pkg.name}:properties`,
-    version: pkg.version,
-    description: "Action button generic object definition",
-  },
-  output: {
-    file: path.resolve(__dirname, "../api-specifications/properties.json"),
-  },
-  parse: {
-    types: {
-      "qae.GenericObjectProperties": {},
+    output: {
+      file: path.resolve(__dirname, "../api-specifications/properties.json"),
+    },
+    parse: {
+      types: {
+        GenericObjectProperties: {
+          url: "https://qlik.dev/apis/json-rpc/qix/schemas#%23%2Fdefinitions%2Fschemas%2Fentries%2FGenericObjectProperties",
+        },
+      },
     },
   },
 };
