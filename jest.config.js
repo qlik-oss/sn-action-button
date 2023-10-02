770f0dfadc7a58fc396182cf131aeae64afaea86
dmh <dmh@qlik.com>
2023-10-02 10:24:57 +0200

    chore: add the typescript dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   testEnvironment: "jest-environment-jsdom",
-  // testEnvironment: "jsdom",
   testRegex: "src/.+\\.(test|spec)\\.[jt]sx?$",
   coverageThreshold: {
     global: {
